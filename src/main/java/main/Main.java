bac3cee8d979b06276cc5879b76e6f962ee3f35e
EnderStudiosDEV <enderianstudios@gmail.com>
2022-01-19 20:51:28 -0500

    code commenting :)

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,13 +5,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
+	// This function is called whenever the plugin is enabled
 	@Override
 	public void onEnable() {
+		// Sends some debug info to the console.
 		Bukkit.getLogger().info("Loading Project Akudur...");
 	}
 	
+	// Same as onEnable but for when it's disabled
 	@Override
 	public void onDisable() {
+		// Sends some debug info to the console.
 		Bukkit.getLogger().info("Unloading Project Akudur...");
 	}
 }
