2cae568f1345272253bf01b1e3f9becdef630336
EnderStudiosDEV <enderianstudios@gmail.com>
2022-01-19 20:50:32 -0500

    plugin essentials :)

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,5 +1,17 @@
 package main;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		Bukkit.getLogger().info("Loading Project Akudur...");
+	}
+	
+	@Override
+	public void onDisable() {
+		Bukkit.getLogger().info("Unloading Project Akudur...");
+	}
 }
