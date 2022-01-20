fc841771bc9f9589a62afd8fde6f156400ec5801
EnderStudiosDEV <enderianstudios@gmail.com>
2022-01-20 16:18:31 -0500

    0.1.1.6

---
+++ src/main/java/commands/RedeemCommand.java
@@ -0,0 +1,17 @@
+package commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class RedeemCommand implements CommandExecutor {
+
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if(label.equalsIgnoreCase("redeem")) {
+			sender.sendMessage("yay! you've run the command!");
+			return true;
+		}
+		return false;
+	}
+
+}
