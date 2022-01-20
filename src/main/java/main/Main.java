a1128c9cbb4d973c33834377f99afcb37460e597
EnderStudiosDEV <enderianstudios@gmail.com>
2022-01-20 16:18:18 -0500

    0.1.1.5

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,13 +3,28 @@ package main;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import commands.RedeemCommand;
+import events.GenericEvents;
+
 public class Main extends JavaPlugin {
 
+	static Main instance;
+	
 	// This function is called whenever the plugin is enabled
 	@Override
 	public void onEnable() {
+		// Define an instance of utils
+		Utils utils = new Utils();
+		
+		// Define an instance of main
+		instance = this;
 		// Sends some debug info to the console.
 		Bukkit.getLogger().info("Loading Project Akudur...");
+		
+		// Initialize commands and events
+		utils.createListener(new GenericEvents());
+		
+		utils.createCommand(new RedeemCommand());
 	}
 	
 	// Same as onEnable but for when it's disabled
---
+++ src/main/java/main/Utils.java
@@ -0,0 +1,15 @@
+package main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.event.Listener;
+
+public class Utils {
+
+	public void createListener(Listener listener) {
+		Bukkit.getPluginManager().registerEvents(listener, Main.instance);
+	}
+	public void createCommand(CommandExecutor cmd) {
+		Main.instance.getCommand("kit").setExecutor(cmd);
+	}
+}
